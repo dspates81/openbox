07dd2e2ebe68d2eaa9cd1b09cd133386d876430c
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-15 12:41:05 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 
 
 
-sudo pacman -S --needed nitrogen libredefender firefox gnome-backgrounds zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables polkit xscreensaver wallutils
+sudo pacman -S --needed 
 
 
