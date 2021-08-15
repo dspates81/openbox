dabdf26f90ff2b6b0104c24f7e26ad90ce17dbef
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-15 13:11:40 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@
 
 
 
-sudo pacman -S --needed nitrogen libredefender zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables xscreensaver wallutils xdg-user-dirs picom polkit gvfs gvfs-smb menumaker tint2 pcmanfm geany terminator xarchiver  firefox lxappearance-obconf lxappearance lxsession menumaker lightdm lightdm-gtk-greeter pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer grub-customizer samba bluez bluez-utils xterm arc-gtk-theme papirus-icon-theme git openssh 
+sudo pacman -S --needed nitrogen libredefender zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables xscreensaver wallutils xdg-user-dirs picom polkit gvfs gvfs-smb samba menumaker tint2 pcmanfm geany terminator xarchiver firefox lxappearance-obconf lxappearance lxsession menumaker lightdm lightdm-gtk-greeter pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer grub-customizer samba bluez bluez-utils xterm arc-gtk-theme papirus-icon-theme git openssh 
 
