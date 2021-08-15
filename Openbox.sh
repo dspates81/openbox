c13906df83d2666606d40ed55e84ab40fa0a2fa7
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-14 21:53:16 -0400

    Update Openbox.sh

--- Openbox.sh
+++ Openbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo pacman -S --needed openbox obconf menumaker xorg-server lightdm lightdm-gtk-greeter pcmanfm tint2 samba nitrogen libredefender firefox gnome-backgrounds firefox menumaker geany terminator xarchiver xterm unzip lxappearance-obconf lxappearance grub-customizer xterm libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer xfce4-pulseaudio-plugin samba bluez bluez-utils arj dpkg zip code htop grub-customizer qemu virt-manager ebtables polkit xscreensaver wallutils systemsettings
+sudo pacman -S --needed openbox obconf menumaker xorg-server lightdm lightdm-gtk-greeter pcmanfm tint2 samba nitrogen libredefender firefox gnome-backgrounds firefox menumaker geany terminator xarchiver xterm unzip lxappearance-obconf lxappearance grub-customizer libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer xfce4-pulseaudio-plugin samba bluez bluez-utils arj dpkg zip code htop qemu virt-manager ebtables polkit xscreensaver wallutils
 
 
 #mmaker -f -t xterm
