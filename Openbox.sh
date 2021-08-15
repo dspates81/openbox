d33253c89b5d2af5f931744ceaf8a7fdc016098f
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-14 21:08:50 -0400

    Update Openbox.sh

--- Openbox.sh
+++ Openbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo pacman -S --needed openbox obconf menumaker xorg-server lightdm lightdm-gtk-greeter pcmanfm tint2 ninja setconf nitrogen libredefender firefox gnome-backgrounds firefox menumaker geany terminator xarchiver xterm unzip lxappearance-obconf lxappearance grub-customizer lightdm lightdm-gtk-greeter xterm libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex pavucontrol pipewire-alsa pipewire-pulse pulseaudio pulseaudio-bluetooth pulseaudio-equailzer pulseaudio-zeroconf pulsemixer xfce4-pulseaudio-plugin samba bluez bluez-utils arj dpkg zip code htop task-manager grub customizer qemu virt-manager ebtables polkit xscreensaver wallutils systemsettings
+sudo pacman -S --needed openbox obconf menumaker xorg-server lightdm lightdm-gtk-greeter pcmanfm tint2 ninja setconf nitrogen libredefender firefox gnome-backgrounds firefox menumaker geany terminator xarchiver xterm unzip lxappearance-obconf lxappearance grub-customizer lightdm lightdm-gtk-greeter xterm libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex pavucontrol pipewire-alsa pipewire-pulse pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer xfce4-pulseaudio-plugin samba bluez bluez-utils arj dpkg zip code htop grub-customizer qemu virt-manager ebtables polkit xscreensaver wallutils systemsettings
 
 
 mmaker -f -t xterm
