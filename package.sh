b4465353bb1fcd4a9b0f2a7406193946af37f212
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-15 12:59:52 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 
 
 
-sudo pacman -S --needed 
+sudo pacman -S --needed nitrogen libredefender zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables xscreensaver wallutils xdg-user-dirs picom polkit gvfs gvfs-smb menumaker tint2 pcmanfm geany terminator xarchiver  firefox lxappearance-obconf lxappearance lxsession menumaker lightdm lightdm-gtk-greeter pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer grub-customizer samba bluez bluez-utils xterm arc-gtk-theme papirus-icon-theme
 
 
