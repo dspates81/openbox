7b12eeeaf28330221a275526c5b721267041405c
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-15 12:35:09 -0400

    Update Openbox.sh

--- Openbox.sh
+++ Openbox.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 
-sudo pacman -S --needed xorg xorg-xinit openbox ttf-dejavu ttf-liberation firefox xterm obconf lxappearance-obconf menumaker tint2 pcmanfm geany terminator xarchiver xorg-server
+sudo pacman -S --needed xorg xorg-xinit openbox ttf-dejavu ttf-liberation firefox xterm obconf lxappearance-obconf menumaker tint2 pcmanfm geany terminator xarchiver xorg-server polkit
 
-
-
-
-#sudo ./openbox/Themes.sh
-#sudo ./openbox/systemctl.sh
