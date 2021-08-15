51a6e5f8910e80f36944a493a9f671e0e8f962a2
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-14 22:50:58 -0400

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+sudo pacman -S --needed nitrogen libredefender firefox gnome-backgrounds zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables polkit xscreensaver wallutils
+
+
