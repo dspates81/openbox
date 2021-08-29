dceada8a92d6acf5cf33bae7aaeb429f209ff314
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-29 13:55:15 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,6 +2,5 @@
 
 
 
-sudo pacman -S --needed nitrogen libredefender zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables xscreensaver wallutils xdg-user-dirs picom polkit gvfs gvfs-smb samba menumaker tint2 pcmanfm geany terminator xarchiver firefox lxappearance-obconf lxappearance lxsession menumaker lightdm lightdm-gtk-greeter pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer grub-customizer samba bluez bluez-utils xterm arc-gtk-theme papirus-icon-theme git openssh pycharm-community-edition
-
-cp /etc/samba/smb.conf
+sudo pacman -S --needed nitrogen libredefender zip unzip libreoffice-fresh libreoffice-extension-texmaths libreoffice-extension-writer2latex arj dpkg code htop qemu virt-manager ebtables xscreensaver wallutils xdg-user-dirs picom polkit gvfs gvfs-smb samba menumaker tint2 pcmanfm geany terminator xarchiver firefox lxappearance-obconf lxappearance lxsession menumaker lightdm lightdm-gtk-greeter pavucontrol pulseaudio pulseaudio-bluetooth pulseaudio-zeroconf pulsemixer grub-customizer samba bluez bluez-utils xterm arc-gtk-theme papirus-icon-theme openssh pycharm-community-edition gparted gedit gnome-disk-utility autoconf fontconfig dotconf dconf pkgconf python-zconfig python-zope-configuration python-json-config python-django-appconf
+  
